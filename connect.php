7adb4993a40a63ffab04ecf79e0159e5cd191ea3
dpres <thepressu@gmail.com>
2017-02-15 12:22:00 +0100

    Test FTPloy

--- connect.php
+++ connect.php
@@ -1,6 +1,13 @@
 <?php
 $host = 'localhost';
+$user = 'a9650370_ngApp1';
+$pass = '123456';
+$base = 'a9650370_ngApp1';
+
+/*
+$host = 'localhost';
 $user = 'pma';
 $pass = '1234';
 $base = 'angular';
+*/
 ?>
